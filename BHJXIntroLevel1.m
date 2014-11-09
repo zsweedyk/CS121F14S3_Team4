158bb24e8ad97b70a214f5ee6fa37d549c39da4a
=Ben Leader <=>
2014-11-09 15:52:56 -0800

    Added intro scenes for level 1

--- BHJXIntroLevel1.m
+++ BHJXIntroLevel1.m
@@ -11,11 +11,14 @@
 
 @implementation BHJXIntroLevel1
 
-
+SKLabelNode *_continueButton;
 SKSpriteNode *_slide1;
 SKSpriteNode *_slide2;
 SKSpriteNode *_slide3;
 SKSpriteNode *_slide4;
+SKSpriteNode *_slide5;
+SKSpriteNode *_slide6;
+SKSpriteNode *_slide7;
 int _countTouches;
 
 -(id)initWithSize:(CGSize)size {
@@ -29,30 +32,41 @@ int _countTouches;
         [self addChild:_slide1];
         
         //Create subsequent slides, but keep them hidden
-        _slide2 = [SKSpriteNode spriteNodeWithImageNamed:@"Volcanobackground.png"];
+        _slide2 = [SKSpriteNode spriteNodeWithImageNamed:@"Intro1Scene2.png"];
         _slide2.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
         [self addChild:_slide2];
-        _slide3 = [SKSpriteNode spriteNodeWithImageNamed:@"Startmenubackground.png"];
+        _slide3 = [SKSpriteNode spriteNodeWithImageNamed:@"Intro1Scene3.png"];
         _slide3.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
         [self addChild:_slide3];
-        _slide4 = [SKSpriteNode spriteNodeWithImageNamed:@"Volcanobackground.png"];
+        _slide4 = [SKSpriteNode spriteNodeWithImageNamed:@"Intro1Scene4.png"];
         _slide4.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
         [self addChild:_slide4];
+        _slide5 = [SKSpriteNode spriteNodeWithImageNamed:@"Intro1Scene5.png"];
+        _slide5.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
+        [self addChild:_slide5];
+        _slide6 = [SKSpriteNode spriteNodeWithImageNamed:@"Intro1Scene6.png"];
+        _slide6.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
+        [self addChild:_slide6];
+        _slide7 = [SKSpriteNode spriteNodeWithImageNamed:@"Intro1Scene7.png"];
+        _slide7.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
+        [self addChild:_slide7];
         
         _slide2.hidden = YES;
         _slide3.hidden = YES;
         _slide4.hidden = YES;
+        _slide5.hidden = YES;
+        _slide6.hidden = YES;
+        _slide7.hidden = YES;
         
         //Button for continuing slide show
-        NSString * continueMessage;
-        continueMessage = @"Continue";
-        SKLabelNode *continueButton = [SKLabelNode labelNodeWithFontNamed:@"TimeNewRomanPSMT"];
-        continueButton.text = continueMessage;
-        continueButton.fontSize = 50;
-        continueButton.fontColor = [UIColor blueColor];
-        continueButton.position = CGPointMake(self.size.width/2, self.size.height/15);
-        continueButton.name = @"Continue";
-        [self addChild:continueButton];
+        
+        _continueButton = [SKLabelNode labelNodeWithFontNamed:@"TimeNewRomanPSMT"];
+        _continueButton.text = @"Continue";
+        _continueButton.fontSize = 50;
+        _continueButton.fontColor = [UIColor blueColor];
+        _continueButton.position = CGPointMake(self.size.width/2, self.size.height/15);
+        _continueButton.name = @"Continue";
+        [self addChild:_continueButton];
         
     }
     return self;
@@ -77,6 +91,20 @@ int _countTouches;
             _slide3.hidden = YES;
             _slide4.hidden = NO;
             _countTouches++;
+        } else if (_countTouches == 3) {
+            _slide4.hidden = YES;
+            _slide5.hidden = NO;
+            _countTouches++;
+        } else if (_countTouches == 4) {
+            _slide5.hidden = YES;
+            _slide6.hidden = NO;
+            _countTouches++;
+        } else if (_countTouches == 5) {
+            _slide6.hidden = YES;
+            _slide7.hidden = NO;
+            _countTouches++;
+            _continueButton.position = CGPointMake(self.size.width/2, self.size.height/3);
+            _continueButton.text = @"Start Level?";
         } else {
             SKTransition *reveal = [SKTransition flipHorizontalWithDuration:0.5];
             
