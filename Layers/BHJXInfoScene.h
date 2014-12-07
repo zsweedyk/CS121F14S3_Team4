4901fc04517222d81f2d7601b87ba5c810532224
王小天 <wxt1994@gmail.com>
2014-12-07 15:44:09 -0800

    Add information scene.

---
+++ Layers/BHJXInfoScene.h
@@ -0,0 +1,13 @@
+//
+//  BHJXInfoScene.h
+//  Layers
+//
+//  Created by 王小天 on 14/12/7.
+//  Copyright (c) 2014年 BHJX. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface BHJXInfoScene : SKScene
+
+@end
---
+++ Layers/BHJXInfoScene.m
@@ -0,0 +1,13 @@
+//
+//  BHJXInfoScene.m
+//  Layers
+//
+//  Created by 王小天 on 14/12/7.
+//  Copyright (c) 2014年 BHJX. All rights reserved.
+//
+
+#import "BHJXInfoScene.h"
+
+@implementation BHJXInfoScene {}
+
+@end
